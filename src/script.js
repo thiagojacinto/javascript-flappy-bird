03fd741552e3dce8108c265a0e0437e1e7f49818
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-28 11:33:52 -0300

    [feat] refactor to objects: bird, background & floor.

--- src/script.js
+++ src/script.js
@@ -16,19 +16,84 @@ const bird = {
   x: 10,
   y: 50,
 
+  draw() {
+    // Drawing inside canvas:
+    context.drawImage(
+      imageSrc,
+      bird.srcX, bird.srcY,     // OriginX, OriginY
+      bird.width, bird.height,  // Width, Height
+      bird.x, bird.y,
+      bird.width, bird.height,
+    );
+  },
+}
+
+const floor = {
+  srcX: 0,
+  srcY: 610,
+  width: 224,
+  height: 112,
+  x: 0,
+  y: canvas.height - 112,
+
+  draw() {
+    context.drawImage(
+      imageSrc,
+      floor.srcX, floor.srcY,     // OriginX, OriginY
+      floor.width, floor.height,  // Width, Height
+      floor.x, floor.y,
+      floor.width, floor.height,
+    );
+    // draw a second time to `extend` it 
+    context.drawImage(
+      imageSrc,
+      floor.srcX, floor.srcY,     // OriginX, OriginY
+      floor.width, floor.height,  // Width, Height
+      (floor.x + floor.width), floor.y,
+      floor.width, floor.height,
+    );
+  },
+}
+
+const background = {
+  srcX: 390,
+  srcY: 0,
+  width: 275,
+  height: 204,
+  x: 0,
+  y: canvas.height - 204,
+
+  draw() {
+    // paint background of `background`
+    context.fillStyle = '#70c5ce';
+    context.fillRect(
+      0, 0,
+      canvas.width, canvas.height
+    );
+
+    context.drawImage(
+      imageSrc,
+      background.srcX, background.srcY,     // OriginX, OriginY
+      background.width, background.height,  // Width, Height
+      background.x, background.y,
+      background.width, background.height,
+    );
+    // draw a second time to `extend` it 
+    context.drawImage(
+      imageSrc,
+      background.srcX, background.srcY,     // OriginX, OriginY
+      background.width, background.height,  // Width, Height
+      (background.x + background.width), background.y,
+      background.width, background.height,
+    );
+  },
 }
 
 // Recursive function to draw
 function loop() {
-
-// Drawing inside canvas:
-context.drawImage(
-  imageSrc,
-  bird.srcX, bird.srcY,     // OriginX, OriginY
-  bird.width, bird.height,  // Width, Height
-  bird.x, bird.y,
-  bird.width, bird.height,
-);
+  background.draw();
+  bird.draw();
+  floor.draw();
 
   // Continous loading the image
   requestAnimationFrame(loop)
