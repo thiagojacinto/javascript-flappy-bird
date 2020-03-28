8e048d57a81ce903f3b0edee22fd9c930f4290e8
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-28 11:15:14 -0300

    [feat] drew bird @ canvas

--- src/script.js
+++ src/script.js
@@ -8,3 +8,30 @@ imageSrc.src = './public/sprites.png';
 const canvas = document.getElementById('game-canvas');
 const context = canvas.getContext('2d');
 
+const bird = {
+  srcX: 0,
+  srcY: 0,
+  width: 34,
+  height: 24,
+  x: 10,
+  y: 50,
+
+}
+
+// Recursive function to draw
+function loop() {
+
+// Drawing inside canvas:
+context.drawImage(
+  imageSrc,
+  bird.srcX, bird.srcY,     // OriginX, OriginY
+  bird.width, bird.height,  // Width, Height
+  bird.x, bird.y,
+  bird.width, bird.height,
+);
+
+  // Continous loading the image
+  requestAnimationFrame(loop)
+}
+
+loop(); // Runs loop function
